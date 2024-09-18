144a9846f7455d34bcfe53d2bec5c53e41ab4b03
Quang Minh <157796411+minhminh12315@users.noreply.github.com>
2024-09-18 14:55:09 +0700

    Minh: ADD admin page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,44 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import Login from './Pages/Login/Login'
-import Register from './Pages/Register/Register'
-import About from './Pages/About/About'
-import axios from 'axios';
-import Header from './Pages/User/Header'
-import Footer from './Pages/User/Footer'
-import Home from './Pages/User/Home'
-import Contact from './Pages/User/Contact'
-import AmbulanceRouting from './Component/AmbulanceRouting/AmbulanceRouting';
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
+import About from "./Pages/About/About";
+import axios from "axios";
+import Header from "./Pages/User/Header";
+import Footer from "./Pages/User/Footer";
+import Home from "./Pages/User/Home";
+import Contact from "./Pages/User/Contact";
+import AmbulanceRouting from "./Component/AmbulanceRouting/AmbulanceRouting";
+import UserContext from "./Context/UserContext";
+import { useNavigate } from "react-router-dom";
+import Hospital from "./Pages/Admin/Hospital";
 
 function App() {
+
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState(null);
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  const submitLogout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+    navigate('/login');
+  };
+
+  useEffect(() => {
+    const savedUser = localStorage.getItem("user");
+    if (savedUser) {
+      setUser(JSON.parse(savedUser));
+    }
+  }, []);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:8000/api/example');
+        const response = await axios.get("http://localhost:8000/api/example");
         setData(response.data);
       } catch (error) {
         setError(error.message);
@@ -32,15 +52,24 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<AmbulanceRouting />} />
-      </Routes>
-      <Footer />
+      <UserContext.Provider value={{ user, setUser }}>
+        <Header user={user} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          {user ? (
+            <button onClick={submitLogout()}>Log out</button>
+          ) : (
+            <>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </>
+          )}
+          <Route path="/hospital" element={<Hospital />} />
+          <Route path="/call-ambulance" element={<AmbulanceRouting />} />
+        </Routes>
+        <Footer />
+      </UserContext.Provider>
     </div>
   );
 }
--- server/routes/api.php
+++ server/routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\YourController;
+use App\Http\Controllers\HospitalController;
+use App\Models\Hospital;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,5 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/check-email', [AuthController::class, 'checkEmail'])->name('check-email');
 
+Route::post('/store-hospital', [HospitalController::class, 'store'])->name('store.hospital');
 
-
-Route::get('/example', [YourController::class, 'example']);
